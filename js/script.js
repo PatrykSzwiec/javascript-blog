6bb34557b86a4903af7820514d1460c93407ef79
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-03-08 20:16:03 +0100

    Add const to find correct article by link href

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
   }); */
 const titleClickHandler = function(event){
     console.log('Link was clicked!');
-    console.log(event);
+    //console.log(event);
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
     for(let activeLink of activeLinks){
@@ -14,8 +14,8 @@ const titleClickHandler = function(event){
     /* [DONE] add class 'active' to the clicked link */
     event.preventDefault();
     const clickedElement = this;
-    console.log('clickedElement:', clickedElement);
-    console.log('clickedElement (with plus): ' + clickedElement);
+    //console.log('clickedElement:', clickedElement);
+    //console.log('clickedElement (with plus): ' + clickedElement);
     clickedElement.classList.add('active');
     /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('article.active');
@@ -25,8 +25,9 @@ const titleClickHandler = function(event){
     /* [DONE] get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute("href");
     console.log(articleSelector);
-    /* [NOT DONE] find the correct article using the selector (value of 'href' attribute) */
-  
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
     /* [NOT DONE] add class 'active' to the correct article */
     
 }

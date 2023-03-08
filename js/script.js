2fc61a2bcfa2a09a84db187b185e0c98facd429f
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-03-08 19:45:40 +0100

    Add script to add class active to the clicked link

--- js/script.js
+++ js/script.js
@@ -7,23 +7,26 @@ const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log(event);
   
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
       }
-    /* add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
+    const clickedElement = this;
+    console.log('clickedElement:', clickedElement);
+    console.log('clickedElement (with plus): ' + clickedElement);
+    clickedElement.classList.add('active');
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('article.active');
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
-    /* remove class 'active' from all articles */
+    /* [NOT DONE] get 'href' attribute from the clicked link */
   
-    /* get 'href' attribute from the clicked link */
+    /* [NOT DONE] find the correct article using the selector (value of 'href' attribute) */
   
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
+    /* [NOT DONE] add class 'active' to the correct article */
 }
   
 const links = document.querySelectorAll('.titles a');

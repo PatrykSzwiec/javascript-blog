db25b7625f955e6b7b56c4a1aa64b80c7e339cf9
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-03-08 20:10:17 +0100

    Add const articleSelector to get href attribute from links

--- js/script.js
+++ js/script.js
@@ -6,13 +6,13 @@
 const titleClickHandler = function(event){
     console.log('Link was clicked!');
     console.log(event);
-  
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
       }
     /* [DONE] add class 'active' to the clicked link */
+    event.preventDefault();
     const clickedElement = this;
     console.log('clickedElement:', clickedElement);
     console.log('clickedElement (with plus): ' + clickedElement);
@@ -22,11 +22,13 @@ const titleClickHandler = function(event){
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
-    /* [NOT DONE] get 'href' attribute from the clicked link */
-  
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute("href");
+    console.log(articleSelector);
     /* [NOT DONE] find the correct article using the selector (value of 'href' attribute) */
   
     /* [NOT DONE] add class 'active' to the correct article */
+    
 }
   
 const links = document.querySelectorAll('.titles a');

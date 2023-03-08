42d90888f00b6e9d6682ac8d3b6b51a63a2636bb
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-03-08 20:18:39 +0100

    Add script to show article clicked in left menu

--- js/script.js
+++ js/script.js
@@ -28,8 +28,8 @@ const titleClickHandler = function(event){
     /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
     console.log(targetArticle);
-    /* [NOT DONE] add class 'active' to the correct article */
-    
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
 }
   
 const links = document.querySelectorAll('.titles a');
